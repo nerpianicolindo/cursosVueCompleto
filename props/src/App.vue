9962af29d7755f564b1b02c6a2a4218d95105763
algonzalez <algonzalez@movisat.com>
2020-08-25 10:31:37 +0200

    first commit

---
+++ props/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :rows="6" :columns="5"/>
+    <Botton @click="handleClick">
+      <i class="fas fa-cat"></i>
+      Botón de ejemplo
+    </Botton>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Botton from './components/Botton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Botton
+  },
+  mehtods: {
+    handleClick(info) {
+      console.log("Click event on the button of the children with: "+ info)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ props/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    {{ rows}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    columns: {
+      type: Number,
+      default: 4
+    },
+    rows: {
+      type: Number,
+      default: 3
+    }
+  },
+  created(){
+    console.log("Columns: " + this.columns + " Rows: " + this.rows);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
