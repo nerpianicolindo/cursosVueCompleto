9629e0ac391facec9fb3fbc3741630d0c3adc33e
algonzalez <algonzalez@movisat.com>
2020-08-26 08:53:22 +0200

    curso nuxtjs

--- props/src/App.vue
+++ props/src/App.vue
@@ -6,10 +6,15 @@
       <i class="fas fa-cat"></i>
       Botón de ejemplo
     </Botton>
+    <div v-if="result" class="content">
+      <p>User ID: {{ result.userId }}</p>
+      <p>Title: {{ result.title }}</p>
+  </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import HelloWorld from './components/HelloWorld.vue'
 import Botton from './components/Botton.vue'
 
@@ -19,10 +24,56 @@ export default {
     HelloWorld,
     Botton
   },
+  data: () => ({
+    result: null
+  }),
+  /* created() {
+     axios.get("https://jsonplaceholder.typicode.com/todos/1").then((result) => {
+      this.result = result.data;
+    }) 
+      // axios.delete("https://jsonplaceholder.typicode.com/todos/1");
+    let post = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    };
+    axios.post("https://jsonplaceholder.typicode.com/posts", post).then((result) => {
+      console.log(result);
+    });
+    axios.put("https://jsonplaceholder.typicode.com/posts/1", post).then((result) => {
+      console.log(result);
+    });
+  }, */
+  /* async created() {
+    let response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    this.result = response.data;
+  } */
   mehtods: {
     handleClick(info) {
       console.log("Click event on the button of the children with: "+ info)
+    },
+    async getTodos() {
+      try {
+        let response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+        console.log(response.data);
+        this.result = response.data; 
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async modifyPost() {
+      let post = {
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+      };
+      let response = await axios.put("https://jsonplaceholder.typicode.com/posts/1", post)
+      console.log(response.data);
     }
+  },
+  created() {
+    this.getTodos();
+    this.modifyPost();
   }
 }
 </script>
---
+++ web-with-nuxtjs/pages/donate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <header>
+      <h3 class="logo">Nuxt Website</h3>
+
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/">About</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/">Contact</nuxt-link>
+        </li>
+      </ul>
+
+    </header>
+
+    <main>
+      <h1>Donate to us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, eos provident, ullam labore eaque ipsa est harum, illum quidem nulla praesentium optio. Odio illum nobis, amet tempore impedit hic ipsam!</p>
+    </main>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
